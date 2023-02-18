58b624a9a1379e2b052de1711ed376bd7fdf817a
Nora <nsorym04@gmail.com>
2023-02-18 13:56:54 +0900

    対象ファイル

---
+++ wp-content/themes/portfolio/index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="WEBデザイナーを目指し、日々猛進する現役専門学生の初ポートフォリオサイト。ネオン（光）をモチーフにしたサイトに仕上げました。" name="description">
+    <link rel="icon" href="img/icon_32x32.ico">
+    <link rel="stylesheet" href="re_css/style.css">
+    <link rel="stylesheet" href="re_css/normalize.css">
+    <title>NishioRyoma</title>
+    <!-- <script src="https://unpkg.com/ionicons@4.1.2/dist/ionicons.js"></script> -->
+    <script>
+        (function(d) {
+          var config = {
+            kitId: 'uca3rxv',
+            scriptTimeout: 3000,
+            async: true
+          },
+          h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+        })(document);
+      </script>
+      </head>
+<body>
+    <div class="loading">
+        <div class="load_container">
+            <img src="img/smileG.png" alt=""><img src="img/smileY.png" alt=""><img src="img/smileR.png" alt="">
+        </div>
+    </div>
+
+
+    <div class="menu">
+        <div class="c_m"><b>CLOSE</b><b>MENU</b></div>
+        <input type="checkbox">
+        <span id="inbox" class="flex"><ion-icon name="bulb-outline"></ion-icon></span>
+    </div>
+    <header class="selectmenu">
+        <div class="menuback flex">
+            <ul class="menu_list">
+                <li><a href="index.html">MAIN</a></li>
+                <li><a href="about.html">ABOUT</a></li>
+                <li><a href="works.html">WORKS</a></li>
+                <li><a href="#contact">CONTACT</a></li>
+            </ul>
+            <div class="rump flex">
+                <img class="in_rump" src="img/rump.png" alt="rump">
+                <img class="in_light" src="img/light.png" alt="light">
+            </div>
+        </div>
+    </header>
+    <div id="stalker"></div>
+    <div class="hide">
+        <section class="main flex">
+        <img src="img/NishioRyoma.png" alt="NishioRyoma">
+    </section>
+    <section class="about flex wrapper">
+        <div class="titleimg titleabout"></div>
+        <div class="bg aboutbg">
+            <div class="about_text">
+                <div class="about_text_my">
+                    
+                    <div class="ab-content">
+                        <p>I AM RYOMA</p>
+                        <p>TAKAYAMA CITY</p>
+                        <div class="bgsmile"><img src="img/smile1.png" alt="smile"></div>
+                    </div>
+                    
+                    <div class="ab-content">
+                        <p>WEB DESIGN</p>
+                        <p>FRONTEND</p>
+                        <div class="bgsmile"><img src="img/smile1.png" alt="smile"></div>
+                    </div>
+
+                    <div class="ab-content">
+                        <p>What’s wrong with</p>
+                        <p>enjoying the moment?</p>
+                        <div class="bgsmile"><img src="img/smile1.png" alt="smile"></div>
+                </div>     
+            </div>
+            <div class="dump"><img src="img/ABC.png" alt="ABC"></div>
+            </div>
+
+            <a href="about.html"><img class="viewmore"src="img/viewmore_button.png" alt="viewmore"></a>
+        </div>
+    </section>
+    <section class="works flex wrapper">
+        <div class="titleimg titleworks"></div>
+        <div class="bg worksbg">
+            <div class="all_portfolio">
+                <div class="instagram port_block">
+                    <div class="in_img"><img src="img/p_henadrick.jpeg" alt="henadrick"><img src="img/p_monburan.jpg" alt="monburan"><img src="img/p_ryoma.jpg" alt="ryoma"></div>
+                    <div class="in_img"><img src="img/works_dejavu.jpg" alt="dejavu"><img src="img/p_season.jpg" alt="season"><img src="img/p_aespa.jpg" alt="aespa"></div>
+                    <img class="arrow ar" src="img/arrowP.png" alt="arrow">
+                    <a href="https://www.instagram.com/rr_ym4/" target="_blank"><img class="link_button"src="img/instagram_button.png" alt="instagram"></a>
+                </div>
+                <div class="portfolio port_block">
+                    <div class="in_img"><img src="img/p_portfolio.jpg" alt="portfolio"><img src="img/p_dejavu.jpg" alt="dejavu"><img src="img/p_thanks.jpg" alt="thanks"></div>
+                    <div class="in_img"><img src="img/p_majan.jpg" alt="majan"><img src="img/p_yubika.jpg" alt="yubika"><img src="img/coming soon.jpg" alt="coming soon"></div>
+                    <img class="arrow2 ar" src="img/arrowO.png" alt="arrow">
+                    <a href="works.html"><img class="link_button"src="img/portfolio_button.png" alt="portfolio"></a>
+                </div>
+            </div>
+            </div>
+        
+    </section>
+    <section class="contact flex" id="contact">
+        <div class="titleimg titlecontact"></div>
+        <div class="bg contactbg">
+            <div class="contact_contents">
+            <div class="contact_sns">
+            <div class="sns instagram">
+                <ion-icon name="logo-instagram"></ion-icon><p>INSTAGRAM</p>
+            </div>
+            <div class="sns twitter">
+                <ion-icon name="logo-twitter"></ion-icon><p>TWITTER</p>
+            </div>
+            <div class="sns mail">
+                <ion-icon name="mail-open-outline"></ion-icon><p>MAIL</p>
+            </div>
+        </div>
+        <p class="OR">OR</p>
+        <div class="contact_button"><a href="contact.html"></a><img src="img/contact_button.png" alt="contact"></div>
+    </div>
+        </div>
+    </section>
+    </div>
+</div>
+</div>
+<style>
+    .aboutbg{
+        z-index: 999;
+    }
+</style>
+    
+    <script
+    src="https://code.jquery.com/jquery-3.6.1.js"
+    integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI="
+    crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/protonet-jquery.inview/1.1.2/jquery.inview.min.js"></script>       
+    <script src="js/main.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+</div>
+
+</html>
